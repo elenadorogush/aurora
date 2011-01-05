5f4a5d5a77ff374346ffcb66a6706505cd597a6a
U-AAKDESK\alexk <alexk@AAKDESK.(none)>
2011-01-04 20:48:47 -0800

    initial

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * @(#) HelloWorld.java
+ */
+
+import java.io.*
+
+
+/*
+ * Testing
+ * @version $Id: $
+ */
+class HelloWorld
+{  
+        public static void main(String args[])
+        {
+           System.out.println("Hello World!");
+        }
+}
+
